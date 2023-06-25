9ab8584f236af2b558e087dc063c2b63ae6736fc
Den Pedosenko <dpedosenko@gmail.com>
2023-06-25 15:50:13 +0300

    Remove useless alias

--- src/main/initializer.go
+++ src/main/initializer.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"ses.genesis.com/exchange-web-service/src/main/config"
-	notification2 "ses.genesis.com/exchange-web-service/src/main/notification"
-	persistent2 "ses.genesis.com/exchange-web-service/src/main/persistent"
-	service2 "ses.genesis.com/exchange-web-service/src/main/service"
+	"ses.genesis.com/exchange-web-service/src/main/notification"
+	"ses.genesis.com/exchange-web-service/src/main/persistent"
+	"ses.genesis.com/exchange-web-service/src/main/service"
 )
 
 const (
@@ -13,7 +13,7 @@ const (
 	fileStoragePath = "src/main/resources/emails.txt"
 )
 
-func initialize() service2.InternalService {
+func initialize() service.InternalService {
 	configLoader := config.NewConfigLoader(configPath)
 
 	ctx, err := configLoader.GetContext()
@@ -22,9 +22,9 @@ func initialize() service2.InternalService {
 		panic(err)
 	}
 
-	notificationService := notification2.NewEmailSender(ctx, notification2.NewSMTPProtocolService())
-	persistentService := persistent2.NewFileStorage(persistent2.NewFileProcessor(fileStoragePath))
-	externalService := service2.NewExternalExchangeAPIController(ctx)
+	notificationService := notification.NewEmailSender(ctx, notification.NewSMTPProtocolService())
+	persistentService := persistent.NewFileStorage(persistent.NewFileProcessor(fileStoragePath))
+	externalService := service.NewExternalExchangeAPIController(ctx)
 
-	return service2.NewMainService(externalService, persistentService, notificationService)
+	return service.NewMainService(externalService, persistentService, notificationService)
 }
